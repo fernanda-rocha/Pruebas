ae0b3776491f8df4bf10bbde7c770cf7e84b3d10
Fernanda Rocha <fernanda.lennon@comunidad.unam.mx>
2018-10-18 23:45:31 -0500

    Add files via upload

---
+++ HanoiTowers.java
@@ -0,0 +1,84 @@
+package mx.unam.fi.tsingsoft20191.clase9;
+
+public class HanoiTower {
+
+	public int n;
+	public int [][] towers;
+
+	public HanoiTower(int n) {
+		this.n = n;
+		towers = new int[n][];
+
+		for (int i=0; i<n; i++) {
+			towers[i] = new int[3];
+
+			for(int j=0;j<3; j++) {
+				towers[i][j] = 0;
+			}
+		}
+
+		for(int i=n-1; i>=0; i--) {
+			towers[i][0] = i+1;
+		}
+
+	}
+
+	public void solve(int n,char from,char inter,char to) {
+		if(n==1) {
+			System.out.println("\n Disco 1 desde " + from + " a " + to);
+			updateTowers(n, from, to);
+			display();
+		}else {
+			solve(n-1, from, to, inter);
+			System.out.println("\n Disco " + n + " desde " + from + " a " + to);
+			updateTowers(n, from, to);
+			display();
+			solve(n-1, inter, from, to);
+		}
+	}
+
+	public int topForTower(char tower) {
+		int i = n-1;
+
+		while(i >= 0 && towers[i][tower - 65] != 0) {
+			i--;
+		}
+		return i;
+	}
+
+		public void updateTowers(int n,char from,char to){
+			int topForTo = topForTower(to);
+			int topForFrom = topForTower(from);
+			towers[topForTo][to - 65] = towers[topForFrom + 1][from - 65];
+			towers[topForFrom + 1][from - 65] = 0;
+	}
+
+		public void display() {
+			System.out.println("\n Torres");
+
+			for(int i = 0; i < n; i++) {
+				for(int j = 0; j < 3; j++) {
+					System.out.print((towers[i][j] == 0 ? " " : towers[i][j]) + "\t");
+				}
+
+				System.out.println();
+			}
+
+			for(int i = 0; i < n; i++) {
+				System.out.print((char)(i+65) + "\t");
+			}
+
+			System.out.println();
+		}
+
+		public static void main(String[] args) {
+			int n = 3;
+			HanoiTower hanoiTower = new HanoiTower(n);
+			hanoiTower.display();
+			hanoiTower.solve(n, 'A', 'B', 'C');
+		}
+
+
+
+
+}
